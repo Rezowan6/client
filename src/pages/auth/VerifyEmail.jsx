8e55dc175615860273c07c56c5c078c31a375c9f
Md Rezowan Miya <mdrezowanmiya6@gmail.com>
2026-04-25 15:30:22 +0600

    change email verify

--- src/pages/auth/VerifyEmail.jsx
+++ src/pages/auth/VerifyEmail.jsx
@@ -3,37 +3,36 @@ import { useNavigate, useParams } from "react-router-dom";
 import AlertPopup from "../../components/alertPopup/AlertPopup.jsx";
 import useAlert from "../../hooks/useAlert.jsx";
 import { verifyEmail } from "../../services/auth/authService.js";
+import Button from './../../components/Button/Button';
 
 const VerifyEmail = () => {
   const { token } = useParams();
   const navigate = useNavigate();
 
-  const { alertData, showAlert, closeAlert, confirmAction } =
-    useAlert();
+  const { alertData, showAlert, closeAlert, confirmAction } = useAlert();
 
   useEffect(() => {
-    let timer;
-
     const verify = async () => {
       try {
         const res = await verifyEmail(token);
         showAlert(res.message || "Verified Successfully");
-
-        timer = setTimeout(() => {
-          navigate("/login");
-        }, 2000);
       } catch (error) {
         showAlert(error?.response?.data?.message || "Error occurred");
       }
     };
 
     verify();
+  }, [token]);
 
-    return () => clearTimeout(timer); // cleanup
-  }, [token, navigate, showAlert,]);
 
+  const userLogin = () => {
+    navigate("/login");
+  }
   return (
     <>
+    <div className="flex justify-center items-center">
+      <Button text="login" type="button" onclickHandle={userLogin} />
+    </div>
       <AlertPopup
         {...alertData}
         onClose={closeAlert}
