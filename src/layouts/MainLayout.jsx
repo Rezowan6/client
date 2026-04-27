e97bfbc59da2a3ead708670605ce89da0b674ae2
Md Rezowan Miya <mdrezowanmiya6@gmail.com>
2026-04-27 11:24:35 +0600

    mainLayout update

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,17 +6,18 @@ import Navbar from "./../components/Navbar/Navbar";
 const MainLayout = () => {
   return (
     <>
-      <div className="flex min-h-screen">
-        <Sidebar />
+<div className="flex flex-col lg:flex-row max-w-7xl mx-auto min-h-screen">
+  <Sidebar />
 
-        <div className="flex-1">
-          {/* <Navbar /> */}
+  <div className="flex-1">
+    {/* <Navbar /> */}
 
-          <main className="mt-20 p-6 container mx-auto">
-            <Outlet />
-          </main>
-        </div>
-      </div>
+    <main className="mt-20 container mx-auto px-4">
+      <Outlet />
+    </main>
+  </div>
+  
+</div>
     </>
   );
 };
